072f8227c5d1fe922dd1a6761daebbe3832a3844
joseeneto <josemnetodev@gmail.com>
2025-09-01 22:35:15 -0300

    feat: Create streaming entity.

---
+++ src/main/java/com/bingebox/entity/Streaming.java
@@ -0,0 +1,20 @@
+package com.bingebox.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_streaming")
+public class Streaming {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+}

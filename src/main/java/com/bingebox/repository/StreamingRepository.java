4f31936a19629218b30aec68d61a2d8449e430d6
joseeneto <josemnetodev@gmail.com>
2025-09-01 22:32:51 -0300

    feat: Create StreamingRepository

---
+++ src/main/java/com/bingebox/repository/StreamingRepository.java
@@ -0,0 +1,7 @@
+package com.bingebox.repository;
+
+import com.bingebox.entity.Streaming;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StreamingRepository extends JpaRepository<Streaming, Long> {
+}

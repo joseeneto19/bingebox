2224198e1db44f8803cc1c786fcaf40dc7f294a2
joseeneto <josemnetodev@gmail.com>
2025-09-04 14:15:26 -0300

    fix: Fix null return error when saving a movie.

--- src/main/java/com/bingebox/service/MovieService.java
+++ src/main/java/com/bingebox/service/MovieService.java
@@ -1,11 +1,13 @@
 package com.bingebox.service;
 
+import com.bingebox.entity.Category;
 import com.bingebox.entity.Movie;
+import com.bingebox.entity.Streaming;
 import com.bingebox.repository.MovieRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,12 +15,28 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
     public Movie save(Movie movie) {
+        movie.setCategories(findCategories(movie.getCategories()));
+        movie.setStreamings(findStreamings(movie.getStreamings()));
         return movieRepository.save(movie);
     }
 
     public List<Movie> findAll() {
         return movieRepository.findAll();
+    }
+
+    public List<Category> findCategories(List<Category> categories) {
+        List<Category> categoriesFound = new ArrayList<>();
+        categories.forEach(category -> categoryService.findById(category.getId()).ifPresent(categoriesFound::add));
+        return categoriesFound;
+    }
+
+    public List<Streaming> findStreamings(List<Streaming> streamings) {
+        List<Streaming> streamingsFound = new ArrayList<>();
+        streamings.forEach(streaming -> streamingService.findById(streaming.getId()).ifPresent(streamingsFound::add));
+        return streamingsFound;
     }
 }

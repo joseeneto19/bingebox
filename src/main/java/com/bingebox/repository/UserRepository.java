1f829fe53397eeaaa6b6b101404aad2acde112c0
joseeneto <josemnetodev@gmail.com>
2025-09-09 15:06:15 -0300

    feat: Create UserRepository and entity User. Add migration for create table user.

--- src/main/java/com/bingebox/repository/UserRepository.java
+++ src/main/java/com/bingebox/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.bingebox.repository;
 
-public interface UserRepository {
+import com.bingebox.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }

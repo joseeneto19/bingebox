e7ba8c568dfd6df917dbce00813214333b2e5981
joseeneto <josemnetodev@gmail.com>
2025-09-09 15:07:02 -0300

    feat: Add user registration logic and create register endpoint

--- src/main/java/com/bingebox/controller/AuthController.java
+++ src/main/java/com/bingebox/controller/AuthController.java
@@ -1,4 +1,30 @@
 package com.bingebox.controller;
 
+import com.bingebox.controller.request.UserRequest;
+import com.bingebox.controller.response.UserResponse;
+import com.bingebox.entity.User;
+import com.bingebox.mapper.UserMapper;
+import com.bingebox.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bingebox/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<UserResponse> register(@RequestBody UserRequest request) {
+        User savedUser = userService.save(UserMapper.toUser(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(UserMapper.toUserResponse(savedUser));
+
+    }
+
 }
--- src/main/java/com/bingebox/service/UserService.java
+++ src/main/java/com/bingebox/service/UserService.java
@@ -1,4 +1,17 @@
 package com.bingebox.service;
 
+import com.bingebox.entity.User;
+import com.bingebox.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }

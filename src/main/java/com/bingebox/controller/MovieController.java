4e9668739bf8a93c5aa81c0e1e1265e7abad96a3
joseeneto <josemnetodev@gmail.com>
2025-09-09 14:37:31 -0300

    feat: Add movieByCategory

--- src/main/java/com/bingebox/controller/MovieController.java
+++ src/main/java/com/bingebox/controller/MovieController.java
@@ -6,10 +6,12 @@ import com.bingebox.entity.Movie;
 import com.bingebox.mapper.MovieMapper;
 import com.bingebox.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/bingebox/movie")
@@ -37,5 +39,30 @@ public class MovieController {
         return movieService.findById(id)
                 .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MovieResponse> update(@PathVariable Long id, @RequestBody MovieRequest request) {
+        return movieService.update(id, MovieMapper.toMovie(request))
+                .map(movie -> ResponseEntity.ok(MovieMapper.toMovieResponse(movie)))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<MovieResponse>> findByCategory(@RequestParam Long category) {
+        return ResponseEntity.ok(movieService.findByCategory(category)
+                        .stream()
+                        .map(MovieMapper::toMovieResponse)
+                        .toList());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Movie> optMovie = movieService.findById(id);
+        if (optMovie.isPresent()) {
+            movieService.delete(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
     }
 }
--- src/main/java/com/bingebox/repository/MovieRepository.java
+++ src/main/java/com/bingebox/repository/MovieRepository.java
@@ -1,9 +1,14 @@
 package com.bingebox.repository;
 
+import com.bingebox.entity.Category;
 import com.bingebox.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
+
+    List<Movie> findMovieByCategories(List<Category> categories);
 }
